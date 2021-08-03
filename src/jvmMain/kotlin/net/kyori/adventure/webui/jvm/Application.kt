badfe9fadf6f41220e220ebd6c18c685353dd1b2
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2021-08-03 15:01:42 +0100

    common, jvm: Misc performance improvements

--- src/jvmMain/kotlin/net/kyori/adventure/webui/jvm/Application.kt
+++ src/jvmMain/kotlin/net/kyori/adventure/webui/jvm/Application.kt
@@ -1,7 +1,15 @@
 package net.kyori.adventure.webui.jvm
 
 import io.ktor.application.Application
+import io.ktor.application.install
 import io.ktor.application.log
+import io.ktor.features.CachingHeaders
+import io.ktor.features.Compression
+import io.ktor.features.deflate
+import io.ktor.features.gzip
+import io.ktor.http.CacheControl
+import io.ktor.http.ContentType
+import io.ktor.http.content.CachingOptions
 import io.ktor.routing.routing
 
 public fun Application.main() {
@@ -10,6 +18,23 @@ public fun Application.main() {
         // enable trace routing if in dev mode
         if (developmentMode) {
             trace { route -> log.debug(route.buildText()) }
+        }
+    }
+
+    install(Compression) {
+        gzip()
+        deflate()
+    }
+
+    install(CachingHeaders) {
+        options { outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS, ContentType.Text.JavaScript ->
+                    CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 3600))
+                ContentType.Image.Any, ContentType.Application.FontWoff ->
+                    CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 86400))
+                else -> null
+            }
         }
     }
 }
