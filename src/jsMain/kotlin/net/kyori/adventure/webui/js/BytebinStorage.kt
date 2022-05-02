a3a603ae8a3cda72bdf27400838ba2e8db18f4b2
Emilia Dreamer <emilia@rymiel.space>
2022-05-02 13:52:16 +0300

    cleanup bytebin storage by removing the boolean toggle
    This would have been tree-shaken away anyway, but it seems the client-side way will be used anyway
    Keeping the server code around, FOR NOW

--- src/jsMain/kotlin/net/kyori/adventure/webui/js/BytebinStorage.kt
+++ src/jsMain/kotlin/net/kyori/adventure/webui/js/BytebinStorage.kt
@@ -3,8 +3,6 @@ package net.kyori.adventure.webui.js
 import kotlinx.browser.window
 import kotlinx.serialization.encodeToString
 import net.kyori.adventure.webui.Serializers
-import net.kyori.adventure.webui.URL_API
-import net.kyori.adventure.webui.URL_MINI_SHORTEN
 import net.kyori.adventure.webui.tryDecodeFromString
 import net.kyori.adventure.webui.websocket.Combined
 import net.kyori.adventure.webui.websocket.Placeholders
@@ -19,49 +17,25 @@ import kotlin.js.json
 
 private const val BYTEBIN_INSTANCE: String = "https://bytebin.lucko.me"
 
-/** Since it's sorta undecided whether to use client- or server-sided bytebin integration, here's just a toggle
- * for it. When using server-side integration, the server essentially just acts pretty much like a direct proxy
- * to bytebin, as there is no additional logic needed. The client could also do all of it on its own much more directly,
- * however there are also some downsides to that...
- */
-private const val CLIENTSIDE_BYTEBIN: Boolean = true
-
 public fun bytebinStore(payload: Combined): Promise<String?> {
-    val request = if (CLIENTSIDE_BYTEBIN) {
-        window.fetch(
-            "$BYTEBIN_INSTANCE/post",
-            RequestInit(
-                method = "POST",
-                headers = Headers(json("Content-Type" to "application/json; charset=UTF-8")),
-                body = Serializers.json.encodeToString(payload)
-            )
+    return window.fetch(
+        "$BYTEBIN_INSTANCE/post",
+        RequestInit(
+            method = "POST",
+            headers = Headers(json("Content-Type" to "application/json; charset=UTF-8")),
+            body = Serializers.json.encodeToString(payload)
         )
-            .then { response -> response.json() }
-            .then { json -> json.unsafeCast<Json>()["key"].unsafeCast<String>() } // :I
-    } else {
-        window.postPacket("$URL_API$URL_MINI_SHORTEN", payload)
-            .then { response -> response.text() }
-            .then { i -> i } // :I
-    }
-
-    return request
+    )
+        .then { response -> response.json() }
+        .then { json -> json.unsafeCast<Json>()["key"].unsafeCast<String>() } // :I
 }
 
 private fun bytebinLoad(code: String): Promise<Combined?> {
     // TODO(rymiel): handle the 404 case
-    val request = if (CLIENTSIDE_BYTEBIN) {
-        window.fetch(
-            "$BYTEBIN_INSTANCE/$code",
-            RequestInit(method = "GET")
-        )
-    } else {
-        window.fetch(
-            "$URL_API$URL_MINI_SHORTEN?code=$code",
-            RequestInit(method = "GET")
-        )
-    }
-
-    return request
+    return window.fetch(
+        "$BYTEBIN_INSTANCE/$code",
+        RequestInit(method = "GET")
+    )
         .then { response -> response.text() }
         .then { text -> Serializers.json.tryDecodeFromString(text) }
 }
